f1db925296fb21da7813157968f174ca4ce41241
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-07 14:33:12 +0200

    editted styling

--- vue3_project/src/App.vue
+++ vue3_project/src/App.vue
@@ -1,30 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <navbar></navbar>
+    <router-view></router-view>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Navbar from "./components/Navbar.vue";
+
+/**
+ * The root component of the application.
+ *
+ * This component serves as the main entry point for the application, rendering
+ * the navigation bar and the router view where different pages will be displayed.
+ *
+ * @component
+ */
+export default {
+  name: "App",
+  components: {
+    /**
+     * Navbar component
+     */
+    Navbar,
+  },
+};
+</script>
+
+<style>
+:root {
+  --primary-dark: #04082e;
+  --primary-medium: #634e4e;
+  --primary-light: #c1e6f7;
+  --primary-accent1: #7a0f3e;
+  --primary-accent2: #efa512;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+  font-family: BDSans-Regular, sans-serif !important;
+  background-color: var(--primary-light);
+  min-height: 100vh;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+* {
+  --tw-ring-color: rgba(53, 209, 123, 0.5);
 }
 </style>
